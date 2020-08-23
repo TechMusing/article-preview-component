5875b333f579c53cbb4cae75ed6029a395a84f05
Tech Musing <techmusing@protonmail.com>
2020-08-23 14:15:49 -0500

    Add social share popup for mobile

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleSocial = () => {
+  const author = document.getElementById("author");
+  const authorSocial = document.getElementById("author-social");
+
+  author.classList.toggle("hidden");
+  authorSocial.classList.toggle("hidden");
+};
